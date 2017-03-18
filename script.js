51faf830cd114361f7e3268ec4784e1de461e1fd
Carles Alcolea <nmaxcom@gmail.com>
2017-03-18 10:11:13 +0100

    convert into IIFE and create the .sweep class

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-window.onload = () =>{
-    var startingX, target, originalValue, dragging;
-    document.onmousedown = (e)=>{
-        if(e.target.localName === "span"){
-            e.preventDefault();
-            target        = e.target;
-            originalValue = e.target.innerHTML;
-            startingX     = e.pageX;
-            dragging      = true;
-        }
+(function(){
+    window.onload = () =>{
+        var $sweeps = document.querySelectorAll('.sweep');
+
+        var startingX, target, originalValue, dragging;
+        document.onmousedown = (e)=>{
+            if(e.target.className === "sweep"){
+                e.preventDefault();
+                target        = e.target;
+                originalValue = e.target.innerHTML;
+                startingX     = e.pageX;
+                dragging      = true;
+            }
+        };
+        document.onmousemove = (e)=>{
+            if(dragging){
+                var moved         = Math.floor((e.pageX - startingX) * 0.01 * originalValue);
+                target.innerHTML  = parseInt(originalValue) + moved;
+                var $result       = document.getElementById('result');
+                var $input1       = document.querySelectorAll('.sweep')[0].innerHTML;
+                var $input2       = document.querySelectorAll('.sweep')[1].innerHTML;
+                $result.innerHTML = parseInt($input1) + parseInt($input2);
+            }
+        };
+        document.onmouseup   = (e) =>{
+            dragging = false;
+        };
     };
-    document.onmousemove = (e)=>{
-        if(dragging){
-            var moved         = Math.floor((e.pageX - startingX) * 0.01 * originalValue);
-            target.innerHTML  = parseInt(originalValue) + moved;
-            var $result       = document.getElementById('result');
-            var $input1       = document.querySelectorAll('span')[0].innerHTML;
-            var $input2       = document.querySelectorAll('span')[1].innerHTML;
-            $result.innerHTML = parseInt($input1) + parseInt($input2);
-        }
-    };
-    document.onmouseup   = (e) =>{
-        dragging = false;
-    };
-};
+})();
+
