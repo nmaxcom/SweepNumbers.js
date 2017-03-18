c965e60ab334832122bf92d5d3e214e1fa19bb7e
Carles Alcolea <nmaxcom@gmail.com>
2017-03-18 07:45:48 +0100

    simplificacion en el layout

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onload = () => {
+window.onload = () =>{
     var startingX, target, originalValue, dragging;
-    document.onmousedown = (e)=> {
-        if(e.target.localName === "span") {
+    document.onmousedown = (e)=>{
+        if(e.target.localName === "span"){
             e.preventDefault();
             target        = e.target;
             originalValue = e.target.innerHTML;
@@ -9,17 +9,17 @@ window.onload = () => {
             dragging      = true;
         }
     };
-    document.onmousemove = (e)=> {
-        if(dragging) {
-            var moved        = Math.floor((e.pageX - startingX) * 0.01 * originalValue);
-            target.innerHTML = parseInt(originalValue) + moved;
-            var $resul       = document.getElementById('resul');
-            var $input1      = document.querySelectorAll('span')[0].innerHTML;
-            var $input2      = document.querySelectorAll('span')[1].innerHTML;
-            $resul.innerHTML = parseInt($input1) + parseInt($input2);
+    document.onmousemove = (e)=>{
+        if(dragging){
+            var moved         = Math.floor((e.pageX - startingX) * 0.01 * originalValue);
+            target.innerHTML  = parseInt(originalValue) + moved;
+            var $result       = document.getElementById('result');
+            var $input1       = document.querySelectorAll('span')[0].innerHTML;
+            var $input2       = document.querySelectorAll('span')[1].innerHTML;
+            $result.innerHTML = parseInt($input1) + parseInt($input2);
         }
     };
-    document.onmouseup   = (e) => {
+    document.onmouseup   = (e) =>{
         dragging = false;
     };
 };
