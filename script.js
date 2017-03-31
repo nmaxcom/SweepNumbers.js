cf8d9b576de59f7e244ecffba95816a791e7019c
Carles Alcolea <nmaxcom@gmail.com>
2017-03-31 06:04:24 +0200

    all E6 code is, padawan

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 /**
  * Nano library to change the value of numbers in the DOM by sweeping them left or right
  *
- * - todo: re-write to 100% ES6
+ * Surround the number you want to activate with a span tag, class "sweep" like so:
+ * <h3> <span class="sweep"> 104 </span> </h3>
+ *
  */
 
 {
@@ -9,19 +11,19 @@
 
     window.onload = function(){
 
-        let $sweeps = document.querySelectorAll('.sweep');
-        if (!$sweeps>0) return;
+        let $sweeps = document.querySelectorAll('span.sweep');
+        if(!$sweeps > 0) return;
 
         for(let i = 0, len = $sweeps.length; i < len; i++){
             console.info($sweeps[i]);
 
-            $sweeps[i].onmouseover = document.body.style.cursor = 'ew-resize';
-            $sweeps[i].onmouseout = document.body.style.cursor = 'default';
+            $sweeps[i].addEventListener('mouseover', () => document.body.style.cursor = 'ew-resize');
+            $sweeps[i].addEventListener('mouseout', () => document.body.style.cursor = 'default');
         }
 
-        var startingX, target, originalValue, dragging;
+        let startingX, target, originalValue, dragging;
         document.onmousedown = (e) =>{
-            if(e.target.className === "sweep"){
+            if(e.target.className === 'sweep'){
                 e.preventDefault();
                 target        = e.target;
                 originalValue = e.target.innerHTML;
@@ -32,11 +34,11 @@
 
         document.onmousemove = (e) =>{
             if(dragging){
-                var moved         = Math.floor((e.pageX - startingX) * 0.01 * originalValue);
+                let moved         = Math.floor((e.pageX - startingX) * 0.01 * originalValue);
                 target.innerHTML  = parseInt(originalValue) + moved;
-                var $result       = document.getElementById('result');
-                var $input1       = document.querySelectorAll('.sweep')[0].innerHTML;
-                var $input2       = document.querySelectorAll('.sweep')[1].innerHTML;
+                let $result       = document.getElementById('result');
+                let $input1       = document.querySelectorAll('.sweep')[0].innerHTML;
+                let $input2       = document.querySelectorAll('.sweep')[1].innerHTML;
                 $result.innerHTML = parseInt($input1) + parseInt($input2);
             }
         };
