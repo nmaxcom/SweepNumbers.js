3ea157017f4e20b79372da6620d2b3c6a3312535
Carles Alcolea <nmaxcom@gmail.com>
2017-04-09 04:21:59 +0200

    :books: all commented and blur fixed

--- SweepNumbers.js
+++ SweepNumbers.js
@@ -4,21 +4,16 @@
  *
  * Surround the number you want to activate with a span tag, class "sweep" like so:
  * <h3><span class="sweep">104</span></h3>
- *
  */
 
 let Sweep = (options = false) =>{
-    /**
-     * Merely for debugging purposes
-     */
-        // function d(value,label){
-        //     console.info(`${label}: ${value}`);
-        //     return value;
-        // }
 
     const inOptions        = {};
-    inOptions.validCursors = ['auto', 'default', 'context-menu', 'help', 'pointer', 'progress', 'wait', 'cell', 'crosshair', 'text', 'vertical-text', 'alias', 'copy', 'move', 'no-drop', 'not-allowed', 'all-scroll', 'col-resize', 'row-resize', 'n-resize', 'e-resize', 's-resize', 'w-resize', 'ns-resize', 'ew-resize', 'ne-resize', 'nw-resize', 'se-resize', 'sw-resize', 'nesw-resize', 'nwse-resize'];
+    // We scan the page once in page load and store all the span.sweep's
     const $sweeps          = document.querySelectorAll('span.sweep');
+    // Browser standard valid cursors
+    inOptions.validCursors = ['auto', 'default', 'context-menu', 'help', 'pointer', 'progress', 'wait', 'cell', 'crosshair', 'text', 'vertical-text', 'alias', 'copy', 'move', 'no-drop', 'not-allowed', 'all-scroll', 'col-resize', 'row-resize', 'n-resize', 'e-resize', 's-resize', 'w-resize', 'ns-resize', 'ew-resize', 'ne-resize', 'nw-resize', 'se-resize', 'sw-resize', 'nesw-resize', 'nwse-resize'];
+    // No span.sweep's? Nothing to do in this page
     if(!$sweeps > 0) return;
 
     let startingX,      // x coordinate of the mouse when started dragging
@@ -26,10 +21,32 @@ let Sweep = (options = false) =>{
         originalNumber, // sweep element's original value
         dragging;       // are we currently dragging?
 
+    // Every SweepNumber has to be ready to react to a variety of events
+    for(let i = 0, len = $sweeps.length; i < len; i++){
+        $sweeps[i].addEventListener('mouseover', mouseoverF);
+        $sweeps[i].addEventListener('mouseout', mouseoutF);
+        $sweeps[i].addEventListener('mousedown', mousedownF);
+        $sweeps[i].addEventListener('click', clickF);
+
+        // These need to be 'listened' by the document
+        document.addEventListener('mousemove', mousemoveF);
+        document.addEventListener('mouseup', mouseupF);
+
+        // We'll also make sure they're sized accordingly
+
+    }
+
+    /**
+     * Simple and easy way to restore the cursor style
+     */
     function restoreIcon(){
         document.body.style.cursor = 'default';
     }
 
+    /**
+     * Actions to take when the mouse is over a SweepNumber.
+     * So far, we change the cursor type to indicate it is sweepable
+     */
     function mouseoverF(){
         if(options.icon && inOptions.validCursors.includes(options.icon))
             document.body.style.cursor = options.icon;
@@ -37,11 +54,19 @@ let Sweep = (options = false) =>{
             document.body.style.cursor = 'ew-resize';
     }
 
+    /**
+     * Cleanup actions when finishing hovering a number, sometimes sweeping it
+     */
     function mouseoutF(){
         if(!dragging)
             restoreIcon();
     }
 
+    /**
+     * The handler for a mousedown event on a sweep number. Prepares the variables to be used
+     * in case of mousedown -> drag
+     * @param event - the element representing the number being interacted
+     */
     function mousedownF(event){
         event.preventDefault();
         target         = event.target;
@@ -50,6 +75,11 @@ let Sweep = (options = false) =>{
         dragging       = true;
     }
 
+    /**
+     * Handler on mousemove checks if there's actual dragging or not. If it is, changes
+     * the number value according to the distance dragged.
+     * @param event - the element representing the number being interacted
+     */
     function mousemoveF(event){
         if(dragging){
             let moved        = Math.floor(event.pageX - startingX);
@@ -57,39 +87,45 @@ let Sweep = (options = false) =>{
         }
     }
 
+    /**
+     * Mouseup event handler. A mouseup stops the dragging gesture at all times.
+     * It restores the mouse icon.
+     */
     function mouseupF(){
         dragging = false;
         restoreIcon();
     }
 
+    /**
+     * This function is called when clicking a sweep number and it converts it into
+     * an input field that's transparent, to alter the number manually and dynamically.
+     * Either pressing 'Enter' or clicking away will turn the input back to a span element.
+     * @param event
+     */
     function clickF(event){
         let $input = document.createElement('input');
         $input.classList.add('sweep');
         $input.value = event.target.innerText;
-        $input.addEventListener('keypress', (event) =>{
-            if(event.key === 'Enter'){
-                let $span = document.createElement('span');
-                $span.classList.add('sweep');
-                $span.innerText = $input.value;
-                $span.addEventListener('mouseover', mouseoverF);
-                $span.addEventListener('mouseout', mouseoutF);
-                $span.addEventListener('mousedown', mousedownF);
-                $span.addEventListener('click', clickF);
-                $input.parentNode.replaceChild($span, event.target);
-            }
+        $input.addEventListener('keypress', event =>{
+            if(event.key === 'Enter') transformToSpan(event);
+        });
+        $input.addEventListener('blur', event =>{
+            transformToSpan(event);
         });
         event.target.parentNode.replaceChild($input, event.target);
         $input.focus();
+        function transformToSpan(event){
+            let $span = document.createElement('span');
+            $span.classList.add('sweep');
+            $span.innerText = $input.value;
+            $span.addEventListener('mouseover', mouseoverF);
+            $span.addEventListener('mouseout', mouseoutF);
+            $span.addEventListener('mousedown', mousedownF);
+            $span.addEventListener('click', clickF);
+            $input.parentNode.replaceChild($span, event.target);
+        }
     }
 
-    for(let i = 0, len = $sweeps.length; i < len; i++){
-        $sweeps[i].addEventListener('mouseover', mouseoverF);
-        $sweeps[i].addEventListener('mouseout', mouseoutF);
-        $sweeps[i].addEventListener('mousedown', mousedownF);
-        $sweeps[i].addEventListener('click', clickF);
-        document.addEventListener('mousemove', mousemoveF);
-        document.addEventListener('mouseup', mouseupF);
-    }
 };
 
 
